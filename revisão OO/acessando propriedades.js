41467c19322c7eac328c39dbade1568fc5edef08
Elias Keven <eliaskeven2@gmail.com>
2017-08-07 08:55:34 -0300

    Revisão sobre orientação a objetos com JavaScript

---
+++ revisão OO/acessando propriedades.js
@@ -0,0 +1,2 @@
+var obj = {animal: "Elefante"}, selecionado = "animal";
+console.log(obj.animal, obj[selecionado]);
---
+++ revisão OO/propriedades chamadas de Métodos.js
@@ -0,0 +1,6 @@
+/*As propriedades que contem funções são normalmente chamadas de métodos
+do valor a que pertencem.*/
+var comer = "Arroz";
+console.log(typeof comer.toUpperCase);
+console.log(comer.toUpperCase());
+console.log(comer.toLowerCase());
