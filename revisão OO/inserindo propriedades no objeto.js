d39d2018387452098291718261828e61e4e0ca02
Elias Keven <eliaskeven2@gmail.com>
2017-08-07 09:15:07 -0300

    Add files via upload

---
+++ revisão OO/inserindo propriedades no objeto.js
@@ -0,0 +1,7 @@
+var dia1 = {
+  esquilo: false,
+  events: ["trabalho", "Tocar em arvores", "Pizza", "Correr", "Assistir televisao"]
+};
+console.log(dia1.esquilo);
+dia1.lobo = false;
+console.log(dia1.lobo);
---
+++ revisão OO/testando métodos com arrays.js
@@ -0,0 +1,9 @@
+var marcas = [];
+marcas.push("Nike");
+marcas.push("A", "Forte");
+/* .log do console.log é uma proprieade com valor de função. Ou seja, ela é um 
+método de console. */
+console.log(marcas);
+console.log(marcas.join(" ")); //Método que une o valor do array em uma string, separando por um caractere de sua escolha.
+console.log(marcas.pop());
+console.log(marcas);
