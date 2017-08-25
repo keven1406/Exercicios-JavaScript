ee13b935879743fcc112b11a100398c73d3828c3
Elias Keven <eliaskeven2@gmail.com>
2017-08-25 08:41:57 -0300

    Update compondo-funcoes.js

--- programação funcional/compondo-funcoes.js
+++ programação funcional/compondo-funcoes.js
@@ -1,4 +1,4 @@
-//	nomeado:: string -> string -> 'string' -> concatenacao das strings
+//	casouCom:: string -> string -> 'string' -> concatenacao das strings
 const casouCom = PrimeiroNome => casou => segundoNome => PrimeiroNome + casou + segundoNome
 const quem = casouCom('Jessica')
 console.log(quem(' casou com ')('Elias'))
