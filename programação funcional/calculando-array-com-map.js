421c764b213945b1143871cf42b13c88595ca15e
Elias Keven <eliaskeven2@gmail.com>
2017-08-25 09:08:42 -0300

    Update calculando-array-com-map.js

--- programação funcional/calculando-array-com-map.js
+++ programação funcional/calculando-array-com-map.js
@@ -1,4 +1,5 @@
 const array = [1,2,3]
+//calcularArray:: -> numero -> novo array
 const calcularArray = array => numero => array.map(incremento => incremento + numero)
 const somar = calcularArray(array)
 console.log(somar(2))
